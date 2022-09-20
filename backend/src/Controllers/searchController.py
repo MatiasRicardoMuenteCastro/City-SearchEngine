23bcaf6fd14359ca54e9b2bf27dadebe5d9ce93a
MatiasRicardoMuenteCastro <matiascastro@outlook.com.br>
2022-09-20 04:11:54 -0300

    Update

--- backend/src/Controllers/searchController.py
+++ backend/src/Controllers/searchController.py
@@ -1,13 +1,14 @@
-#from sklearn.feature_extraction.text import CountVectorizer
 from flask import Blueprint,request
 from unidecode import unidecode
+import random
+import pandas as pd
+import numpy as np
 import string 
 import difflib
-import pandas as pd
 import requests
 import json
 
-API_KEY = "9b6a3807991240c07ccb2d1c93d0b23ee6d4b33c8c2cb32fc2e79e707f982572"
+API_KEY = "c1d6039b74a493383fd998725a89b7ac2a331ebc23d4aaeba94700b0b8dc0c1a"
 
 searchBP = Blueprint('search',__name__)
 
@@ -21,9 +22,8 @@ def searcEngine():
         searchCity["cidade"]
     except:
         return json.dumps({"error":"JSON mal formatado"}),400
-
-    municipiosDF = pd.read_excel("./backend/src/Dataset/Municipios.xlsx")
     
+
     searchCity["cidade"] = searchCity["cidade"].strip()
     
     if searchCity["UF"] == "":
@@ -31,62 +31,38 @@ def searcEngine():
     
     if searchCity["cidade"] == "":
         return json.dumps({"error":"Digite o nome de um município"}),400
-
-    UFDataset = municipiosDF.query("UF == '"+searchCity["UF"]+"'")
-    print(UFDataset)
-
-    cityUser = "São Paulo"
-    cityDF = "São Paulo"
-
-    #Normalização das strings
-    cityUser = cityUser.lower()
-    cityDF = cityDF.lower()
-
-    cityUser = unidecode(cityUser)
-    cityUser = cityUser.translate(str.maketrans('', '',string.punctuation))
-
-    cityDF = unidecode(cityDF)
-    cityDF = cityDF.translate(str.maketrans('', '',string.punctuation))
     
-    print(cityUser, cityDF)
-
-    sequence = difflib.SequenceMatcher(isjunk= None, a = cityUser, b = cityDF)
-    difference = sequence.ratio()
-    difference = round(difference,2)
-    print(difference) 
-
-    """
-    n = 1
-
-    separator = CountVectorizer(analyzer = "word",ngram_range = (n,n))
-
-    separadorInt = separator.fit([cidadeUser,cidadeDF]).vocabulary_
-
-    counts = CountVectorizer(analyzer="word",ngram_range=(n,n))
+    municipiosDF = pd.read_excel("./backend/src/Dataset/Municipios.xlsx")
     
-    n_grams = counts.fit_transform([cidadeUser,cidadeDF])
+    UFDataset = municipiosDF.query("UF == '"+searchCity["UF"]+"'").reset_index(drop = True)
+
+    if UFDataset.shape[0] == 0:
+        return json.dumps({"error":"Municipio não encontrado"}),404
+
+    cityUser = searchCity["cidade"]
+
+    differenceArray = []
+    for cityDF in UFDataset["Nome do Municipio"]:
+        #Normalização das strings
+        cityUser = cityUser.lower()
+        cityDF = cityDF.lower()
+
+        cityUser = unidecode(cityUser)
+        cityUser = cityUser.translate(str.maketrans('', '',string.punctuation))
+
+        cityDF = unidecode(cityDF)
+        cityDF = cityDF.translate(str.maketrans('', '',string.punctuation))
+        
+        sequence = difflib.SequenceMatcher(isjunk= None, a = cityUser, b = cityDF)
+        difference = sequence.ratio()
+        difference = round(difference,2)
+        differenceArray.append(difference)
     
-    vocab2int = counts.fit([cidadeUser,cidadeDF]).vocabulary_
+    differenceArray = np.asarray(differenceArray)
 
-    n_grams_array = n_grams.toarray()
+    response = UFDataset["Nome do Municipio"][differenceArray.argmax()]
 
-    print("Vetor de n-gramas:\n\n",n_grams_array)
-    print()
-    print("Dicionario de n-gramas:",vocab2int)
-
-    n_grams.toarray()
-
-    intersection_list = np.amin(n_grams.toarray(), axis = 0)
-
-    intersection_count = np.sum(intersection_list)
-
-    index_A = 0
-
-    A_count = np.sum(n_grams.toarray()[index_A])
-    
-    print(intersection_count/A_count)
-    """
-    return json.dumps({"error":"a"})
+    return json.dumps({"UF":searchCity["UF"],"Success":response})
 
 @searchBP.route("/events",methods = ["GET"])            
 def events():
@@ -109,7 +85,7 @@ def events():
 
         jsonReturn = {"Eventos": results["events_results"],"Mais_Eventos":results["more_events_link"]}
     except:
-        return json.dumps({"Error":"O lugar pesquisado não tem eventos disponiveis"}),404
+        return json.dumps({"error":"O lugar pesquisado não tem eventos disponiveis"}),404
     
     return json.dumps(jsonReturn),200
 
@@ -133,22 +109,77 @@ def places():
     if searchCity["searchLocal"] == "":
         return json.dumps({"error":"Digite algum local que deseja vistar (Ex: Restaurantes, Praças ou Museus)"}),400
 
+    response = requests.get(f"https://serpapi.com/search.json?engine=google_maps&q="+searchLocal["searchLocal"]+" em "+searchCity["cidade"]+"&google_domain=google.com&hl=pt&type=search&api_key="+API_KEY)
+    results = response.json()
+
+    if results.get("error") != None:
+        return json.dumps({"error":"Não foi encontrado nenhum "+searchLocal["searchLocal"]+" em "+searchCity["cidade"]}),404
+    
+    return json.dumps({"Lugares":results["local_results"]}),200
+    
+@searchBP.route("/population",methods = ["GET"])
+def population():
+    searchCity = request.get_json(force=True)
 
     try:
-        response = requests.get(f"https://serpapi.com/search.json?engine=google_maps&q="+searchLocal["searchLocal"]+" em "+searchCity["cidade"]+"&google_domain=google.com&hl=pt&type=search&api_key="+API_KEY)
-        results = response.json()
-
-        return json.dumps({"Lugares":results["local_results"]}),200
+        searchCity["UF"]
+        searchCity["cidade"]
     except:
-        return json.dumps({"error":"Municipio pesquisado, ou o lugar que você pesquisou não encontrado"}),400
+        return json.dumps({"error":"JSON mal formatado"}),400
     
+    populationDF = pd.read_excel("./backend/src/Dataset/População.xlsx")
 
+    UFDataset = populationDF.query("UF == '"+searchCity["UF"]+"'").reset_index(drop = True)
+
+    if UFDataset.shape[0] == 0:
+        return json.dumps({"error":"Municipio não encontrado"}),404
+    
+    line = 0
+    searchBool = False
+
+    for i in UFDataset["NOME DO MUNICÍPIO"]:
+        if i == searchCity["cidade"]:
+            searchBool = True
+            population = UFDataset["POPULAÇÃO ESTIMADA"][line]
+            return json.dumps({"success": population}),200
+        line = line+1
+    
+    if searchBool == False:
+        return json.dumps({"error":"A cidade digitada não foi encontrada"}),404
+
+@searchBP.route("/city-image",methods = ["GET"])
+def imagesCity():
+    searchCity = request.get_json(force=True)
+
+    try:
+        searchCity["cidade"]
+    except:
+        return json.dumps({"error":"JSON mal formatado"}),40
+
+    search = searchCity["cidade"]
+
+    imageLink = requests.get(f"https://serpapi.com/search.json?engine=google&q=Imagem+de+"+search+"&location=Brazil&google_domain=google.com.br&gl=br&hl=pt&api_key="+API_KEY)
+
+    imageResponse = imageLink.json()
+
+    if imageResponse.get("error") != None:
+        return json.dumps({"error":"Não foi encontrada nenhuma imagem"}),404
+
+    image = imageResponse["inline_images"]
+        
+    randomImage = random.randrange(0,len(image))
+
+    sendImage = image[randomImage]["original"]
+
+    return json.dumps({"Success":sendImage}),200
 
 
 @searchBP.route("/weather", methods = ["GET"])
 def weather():
-    return json.dumps({"Success":"Clima legal"}),200
+    return json.dumps({"Success":"Clima maneiro"}),200
 
-@searchBP.route("/population",methods = ["GET"])
-def population():
-    return json.dumps({"Success":"População maneira"}),200
+@searchBP.route("/safety",methods = ["GET"])
+def safety():
+    return json.dumps({"Success":"Segurança maneira"}),200
+
+
