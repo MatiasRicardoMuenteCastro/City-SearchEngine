6d7a5b5225abc7a127fdb7f6a9cb7ce1f5675ab6
MatiasRicardoMuenteCastro <matiascastro@outlook.com.br>
2022-10-10 18:38:29 -0300

    s

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask_cors import CORS
 import os
 
 allowed_domain = '*'
-port = int(os.environ.get("PORT", 3001))
+port = int(os.environ.get("PORT", 5000))
 
 app = Flask(__name__)
 cors = CORS(app, resources = {r'*':{'origins':allowed_domain}})
