4616dc26cbb32acb493a46fff44e41b8f1ea73ca
MatiasRicardoMuenteCastro <matiascastro@outlook.com.br>
2022-10-10 18:34:00 -0300

    x4

--- app.py
+++ app.py
@@ -2,13 +2,15 @@ from src.Controllers.searchController import searchBP
 
 from flask import Flask
 from flask_cors import CORS
+import os
 
 allowed_domain = '*'
+port = int(os.environ.get("PORT", 3001))
 
 app = Flask(__name__)
 cors = CORS(app, resources = {r'*':{'origins':allowed_domain}})
 
 app.register_blueprint(searchBP)
 
-app.run(host = "0.0.0.0",port = "49184")
+app.run(host = "0.0.0.0",port = port)
 
